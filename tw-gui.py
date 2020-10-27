ee4bcf8e1b35f6f4f2b2a54d2a6ea6f9b649b947
Ben Mason <locutus@the-collective.net>
2020-10-27 17:02:06 -0400

    Input validation  fixes tracking added tag dropdown

--- tw-gui.py
+++ tw-gui.py
@@ -9,28 +9,57 @@ encoding = 'utf-8'
 debug=False
 theme="Dark Grey 4"
 
-'''
-    Executing TW CLI
+my_tags = ["", "status", "usergroup", "teammeeting" ]
 
-'''
+
+def validate_date(date_text):
+    return_val = False
+
+    try:
+        datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        return_val = True
+        
+    return return_val
+
+def validate_time(time_text):
+    return_val = False
+
+    try:
+        datetime.strptime(time_text, '%H:%M')
+    except ValueError:
+        return_val = True
+        
+    return return_val
+
+''' Executing TW CLI '''
 def call_tw(tw_command, starttime='', stoptime='', date='', taskdesc=''):
     cli = ['timew']
 
     if tw_command == 'startnow':
-        cli.extend(["start", taskdesc])
+        cli.extend(["start"])
+        cli.extend(taskdesc)
     elif tw_command == 'meeting':
         taskdesc = get_calendar_entry()
-        cli.extend(["start", taskdesc])
+        cli.extend(["start"])
+        cli.extend(taskdesc)
     elif tw_command == 'starttime':
-        cli.extend(["start", starttime, taskdesc])
+        cli.extend(["start", starttime])
+        cli.extend(taskdesc)
     elif tw_command == 'track':
         if date != '':
-            starttime = date + "T" + startime
+            starttime = date + "T" + starttime
             stoptime = date + "T" + stoptime
-        cli.extend(['track' , starttime, ' - ', stoptime, taskdesc])
-    elif tw_command == 'stop':
+        cli.extend(['track' , starttime, '-', stoptime])
+        cli.extend(taskdesc)
+    elif tw_command == 'stop':   
         cli.append("stop")
-        
+    elif tw_command == 'continue':
+        cli.append("continue")
+    
+    if (debug): 
+        print(cli)
+
     process = subprocess.Popen(cli, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
 
@@ -39,9 +68,7 @@ def call_tw(tw_command, starttime='', stoptime='', date='', taskdesc=''):
 
     return stdout
 
-'''
-    Get current calendar
-'''
+''' Get current calendar '''
 def get_calendar_entry():
 
     cli = ['/usr/local/bin/icalbuddy',
@@ -56,34 +83,35 @@ def get_calendar_entry():
 
     if (debug): 
         print (cli)
+
     process = subprocess.Popen(cli, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
 
     if (debug): 
         print(stdout)
+        print(stderr)
 
     return str(stdout, encoding)
 
 
-'''
-Main Function
-'''
+''' Main Function '''
 def main():
 
-    fields = [ "date", "starttime", "stoptime", "taskdesc" ]
+    fields = [ "date", "starttime", "stoptime", "taskdesc", "tag" ]
 
     sg.theme(theme)
     # Define the window's contents
     layout = [  
             [ sg.Text("Task:"), sg.Input(key="taskdesc") ],
             #[ sg.Text("Tag:"), sg.Input(key="tag", size=(10,1)) ],
+            [ sg.Text("Tag:"), sg.Combo(my_tags, key="tag", size=(10,1)) ],
             [ sg.Frame(layout=[
                 [ sg.Text("Start Time:"), sg.Input(key="starttime", size=(12,1)), sg.Text("EX: 15:00") ],
                 [ sg.Text("Stop time:"), sg.Input(key="stoptime", size=(12,1)), sg.Text("EX: 15:00") ],
                 [ sg.Text("Date:"), sg.Input(key="date", size=(12,1)), sg.Text("EX: 2020-10-01") ]
             ], title='Date')],
             [ sg.Text("")],
-            [ sg.Button('Start Meeting'), sg.Button('Track') ],
+            [ sg.Button('Start Meeting'), sg.Button('Track'), sg.Button('Continue') ],
             [ sg.Button('Start'), sg.Button('Stop'), sg.Button('Curr Running') ],
             [ sg.Text(size=(40,1), key='status_result') ],
             [ sg.MLine(key="cliout", size=(40,8)) ]
@@ -101,10 +129,24 @@ def main():
         if event == sg.WINDOW_CLOSED or event == 'Quit':
             break
 
-        # if values['tag'] != '':
-        #     task_description = '"' + values['taskdesc'] + '"' + ', "' + values['tag'] + '"'
-        # else:
-        task_description = '"' + values['taskdesc'] + '"'
+        if values['date'] != '':
+            if (validate_date(values['date'])):
+                sg.popup('Invalid date please use format "YYYY-MM-DD" data entered:', values['date'])
+                continue
+        if values['starttime'] != '':
+            if (validate_time(values['starttime'])):
+                sg.popup('Invalid date please use format "HH:MM" data entered:', values['starttime'])
+                continue
+        if values['stoptime'] != '':
+            if (validate_time(values['stoptime'])):
+                sg.popup('Invalid date please use format "HH:MM" data entered:', values['stoptime'])
+                continue
+
+
+        if values['tag'] != '':
+            task_description = [ '"' + values['taskdesc'] + '"', values['tag'] ]
+        else:
+            task_description = [ '"' + values['taskdesc'] + '"' ]
 
         #
         # Button Logic
@@ -123,17 +165,22 @@ def main():
                     taskdesc=task_description)
                 result_display = "Started: " + values['taskdesc']
         elif event == 'Track':
-            result = call_tw('track', 
+            result = call_tw('track',
+                date=values['date'],
                 starttime=values['starttime'], 
                 stoptime=values['stoptime'], 
                 taskdesc=task_description)
+            result_display = "Tracked: " + values['taskdesc']
         elif event == 'Stop':
-            if (values['starttime'] != ''):
+            if (values['stoptime'] != ''):
                 result = call_tw('stop', 
                     stoptime=values['stoptime'])
             else:
                 result = call_tw('stop')
             result_display = "Stopped Tracking"
+        elif event == 'Continue':
+            result = call_tw('continue')
+            result_display = "Continuing last Task"
         else:
             result = call_tw('running')
             result_display = "See Results"
