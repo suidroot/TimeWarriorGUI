484b55d9fd03957f79364f7b11ff2a7923484ee4
Ben Mason <locutus@the-collective.net>
2022-01-14 18:17:26 -0500

    added configuration file

---
+++ config.py
@@ -0,0 +1,20 @@
+
+import logging
+
+# GUI Astetics
+THEME = "Dark Grey 4"
+GLOBAL_FONT = "Any 11"
+
+
+# Enable Debug output
+#LOGGING_LEVEL = logging.ERROR # DEBUG
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_FORMAT = '[%(levelname)s] %(asctime)s - %(funcName)s %(lineno)d - %(message)s'
+
+# Global Constants
+ENCODING = 'utf-8'
+
+CLI_BASE_COMMAND = 'timew'
+ICALBUDDY_LOCATION = '/usr/local/bin/icalbuddy'
+ICALBUDDY_CALENDAR='Calendar'
+ICALBUDDY_ENABLE = True
